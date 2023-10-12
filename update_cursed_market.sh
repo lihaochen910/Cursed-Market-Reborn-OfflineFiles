415f953add10670ccdba7e63074e91032c2dcd7f
Kanbaru <lihaochen910@hotmail.com>
2023-10-12 11:00:31 +0800

    2023-10-12 11:00:31

---
+++ update_cursed_market.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ~/GitWorkspace/Cursed-Market-Reborn-OfflineFiles
+curl -O "https://cursed.cranchpalace.info/DaylightGame/files/{market,marketDlc,dbdCharacterData,singleDbdCharacterData,dbdItemsList,catalog,killSwitch}.json"
+
+current_time="$(date '+%Y-%m-%d %H:%M:%S')"
+echo $current_time
+
+if [ -n "$(git status -s)" ]; then
+	# 有变动 上传
+	echo " detect file changed."
+
+	git add --all
+	git commit -m $current_time
+	git push origin master
+
+else
+	echo "no change, pass."
+fi
